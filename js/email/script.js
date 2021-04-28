deaa03da056d9c665277fcd06c0f63d62cc7d371
fmarabini93 <fmarabini93>
2021-04-28 14:58:40 +0200

    email-with-css

--- js/email/script.js
+++ js/email/script.js
@@ -10,7 +10,7 @@ for (i=0; i < emailList.length; i++) {
 }
 
 if (invited == 1) {
-    document.getElementById("response").innerHTML = "Congratulations, you're invitation is confirmed! See you at the party (please don't forget to wear the mask!!!)"
+    document.getElementById("response").innerHTML = "Congrats! You're invitation is confirmed! See you at the party (please don't forget to wear the mask!!!)";
 } else {
-    document.getElementById("response").innerHTML = "Sadly we can't find you're invitation anywhere. Please check again if you think you've wrongly written your email address, and feel free contact us if you wish to have further information. Thanks"
+    document.getElementById("response").innerHTML = "Sadly we can't find you're invitation anywhere. Please check again if you think you've wrongly written your email address, and feel free contact us if you wish to have further information. Thanks";
 }
