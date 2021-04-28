d9b1e6b34a0043c8a1b1b790680f550e47f3ef7b
fmarabini93 <fmarabini93>
2021-04-28 15:33:04 +0200

    general-touch-ups

---
+++ email/js/script.js
@@ -0,0 +1,16 @@
+var email = prompt("Please insert your email address to see if you're in the list")
+
+var emailList = ["etabeta@yahoo.it", "alfabeto@gmail.com", "enigmista@outlook.it", "mappamondo@icloud.com", "attaccapanni@live.com"]
+
+for (i=0; i < emailList.length; i++) {
+    if (email === emailList[i]) {
+        var invited = 1;
+        i = emailList.length;
+    }
+}
+
+if (invited == 1) {
+    document.getElementById("response").innerHTML = "Congrats! You're invitation is confirmed! See you at the party <i class='fas fa-2x fa-glass-cheers'></i> (please don't forget to wear the mask!!!)";
+} else {
+    document.getElementById("response").innerHTML = "Sadly we can't find you're invitation anywhere <i class='far fa-2x fa-frown'></i> Please check again if you think you've wrongly written your email address, and feel free contact us if you wish to have further information. Thanks";
+}
