3f4507d6ee57e5fb2a08eb39ca18f84abb69f2dd
fmarabini93 <fmarabini93>
2021-04-28 14:31:03 +0200

    base-dice

---
+++ js/dice/script.js
@@ -0,0 +1,10 @@
+var pcLaunch = Math.floor(Math.random()*6 + 1);
+var userLaunch = Math.floor(Math.random()*6 + 1);
+
+if (pcLaunch > userLaunch) {
+    document.getElementById("result").innerHTML = "Sorry, PC won "+pcLaunch+" to "+userLaunch;
+} else if (pcLaunch < userLaunch) {
+    document.getElementById("result").innerHTML = "Congrats! You won "+userLaunch+" to "+pcLaunch;
+} else {
+    document.getElementById("result").innerHTML = "Draw! Please refresh the page"
+}
